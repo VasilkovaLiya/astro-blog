b142a677a4e52d81360c0ee74d634cf034af1a85
VasilkovaLiya <koodoo@bk.ru>
2025-07-03 15:02:16 +0500

    fix defineConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://miniblog-67c.pages.dev',
-	integrations: [mdx(), sitemap()],
+	output: 'static',
+	//site: 'https://miniblog-67c.pages.dev',
+	//integrations: [mdx(), sitemap()],
 });
