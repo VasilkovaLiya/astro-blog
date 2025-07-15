8b091abae28efe5e4fcfeb477f5937dcb6a7c058
VasilkovaLiya <koodoo@bk.ru>
2025-07-15 17:38:40 +0500

    add scss, react // add blogPost, image components

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 //import { markdown }from '@astrojs/markdown-remark';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://miniblog-67c.pages.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), react()],
   markdown: {
     remarkPlugins: [],
     rehypePlugins: [],
